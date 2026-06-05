450d049b7b1feb70042f6d88ec140f091937e975
Saumya Pandey <saumyap271@gmail.com>
2026-06-05 11:43:57 +0530

    Update security.py

--- backend/security.py
+++ backend/security.py
@@ -17,11 +17,10 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
+    return plain_password == hashed_password
 
 def get_password_hash(password):
-    return pwd_context.hash(password)
-
+    return password
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
